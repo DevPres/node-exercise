2d60f132f5207d093fd504cca967258fd26830df
Dario <dariopresdev@protonmail.com>
2023-02-23 06:59:14 +0100

    error

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const registerController = require('../controllers/registration');
+const registerController = require('../controllers/registration.js');
 
-router.post('/', registerController.handleRegistration);
+router.get('/', registerController.handleRegistration);
+
 
 module.exports = router;
+
