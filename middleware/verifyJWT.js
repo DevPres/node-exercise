f43183ac6f3f78ee137f3adc51d4d8f696ffa079
Dario <dariopresdev@protonmail.com>
2023-02-23 06:58:54 +0100

    fixed middleware

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-
+// middleware for verifying JWT token and inject user id in req object
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     if (!authHeader) return res.sendStatus(401);
-    console.log(authHeader); // Bearer token
-    const token = authHeader.split(' ')[1];
+    const token = authHeader;
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
             if (err) return res.sendStatus(403); //invalid token
-            req.user = decoded.username;
+            //inject ttoken
+            req.user_id = decoded.id;
             next();
         }
     );
